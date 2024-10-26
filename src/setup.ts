e6daae561a06cdb1f3019341c10624331ac34dce
ErwannRousseau <erwann.rousseau@icloud.com>
2024-10-26 18:14:01 +0200

    feat: added update script to update biome version and schema version in the biome.json

--- src/setup.ts
+++ src/setup.ts
@@ -1,51 +1,26 @@
 #!/usr/bin/env node
 
-import { execSync } from "node:child_process";
 import * as fs from "node:fs";
 import * as path from "node:path";
 import readline from "node:readline";
 import { fileURLToPath } from "node:url";
 import chalk from "chalk";
-
-const detectPackageManager = (): string => {
-  const hasYarn = fs.existsSync(path.join(process.cwd(), "yarn.lock"));
-  const hasPnpm = fs.existsSync(path.join(process.cwd(), "pnpm-lock.yaml"));
-  const hasBun = fs.existsSync(path.join(process.cwd(), "bun.lockb"));
-
-  if (hasYarn) return "yarn";
-  if (hasPnpm) return "pnpm";
-  if (hasBun) return "bun";
-  return "npm";
-};
-
-const installBiome = (packageManager: string) => {
-  // biome-ignore lint/suspicious/noConsoleLog:
-  console.log(
-    chalk.blue(`🦋 Installing @biomejs/biome using ${packageManager}...`),
-  );
-
-  const command = {
-    npm: "npm install --save-dev --save-exact @biomejs/biome",
-    yarn: "yarn add --dev --exact @biomejs/biome",
-    pnpm: "pnpm add --save-dev --save-exact @biomejs/biome",
-    bun: "bun add --dev --exact @biomejs/biome",
-  }[packageManager];
-
-  if (!command) {
-    console.error(chalk.red(`Unknown package manager: ${packageManager}`));
-    process.exit(1);
-  }
-
-  execSync(command, { stdio: "inherit" });
-};
+import {
+  detectPackageManager,
+  installBiomeLatest,
+  packageJsonPath,
+  readJsonFile,
+  runFormart,
+  writeJsonFile,
+} from "./utils";
 
 const copyBiomeConfig = () => {
   const sourcePath = path.resolve(
     path.dirname(fileURLToPath(import.meta.url)),
     "biome.json",
   );
-  const destinationPath = path.resolve(process.cwd(), "biome.json");
 
+  const destinationPath = path.resolve(process.cwd(), "biome.json");
   if (fs.existsSync(destinationPath)) {
     // biome-ignore lint/suspicious/noConsoleLog:
     console.log(chalk.yellow("🌝 biome.json already exists, skipping copy."));
@@ -58,14 +33,6 @@ const copyBiomeConfig = () => {
 };
 
 const addScriptsToPackageJson = async () => {
-  const packageJsonPath = path.resolve(process.cwd(), "package.json");
-  if (!fs.existsSync(packageJsonPath)) {
-    console.error(
-      chalk.red("🚨 No package.json found in the current directory."),
-    );
-    process.exit(1);
-  }
-
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -106,7 +73,7 @@ const addScriptsToPackageJson = async () => {
     return;
   }
 
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+  const packageJson = readJsonFile(packageJsonPath);
 
   const newScripts = {
     check: "biome check",
@@ -116,7 +83,7 @@ const addScriptsToPackageJson = async () => {
   };
 
   packageJson.scripts = { ...packageJson.scripts, ...newScripts };
-  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+  writeJsonFile(packageJsonPath, packageJson);
   // biome-ignore lint/suspicious/noConsoleLog:
   console.log(chalk.green("✍️  Scripts added to package.json."));
   rl.close();
@@ -124,9 +91,10 @@ const addScriptsToPackageJson = async () => {
 
 const main = async () => {
   const packageManager = detectPackageManager();
-  installBiome(packageManager);
+  installBiomeLatest(packageManager);
   copyBiomeConfig();
   await addScriptsToPackageJson();
+  runFormart();
   // biome-ignore lint/suspicious/noConsoleLog:
   console.log(chalk.green("🎉 Biome setup completed successfully!"));
 };
---
+++ src/update.ts
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+import * as fs from "node:fs";
+import * as path from "node:path";
+import chalk from "chalk";
+import {
+  detectPackageManager,
+  installBiomeLatest,
+  packageJsonPath,
+  readJsonFile,
+  runFormart,
+  writeJsonFile,
+} from "./utils";
+
+const updateBiomeSchema = () => {
+  const packageJson = readJsonFile(packageJsonPath);
+
+  let biomeVersion = packageJson.devDependencies["@biomejs/biome"];
+  if (biomeVersion.startsWith("^")) {
+    biomeVersion = biomeVersion.substring(1);
+  }
+
+  const biomeJsonPath = path.resolve(process.cwd(), "biome.json");
+
+  if (!fs.existsSync(biomeJsonPath)) {
+    // biome-ignore lint/suspicious/noConsoleLog: <explanation>
+    console.log(chalk.yellow("🌝 biome.json not found, skipping update."));
+    process.exit(1);
+  }
+
+  const biomeJson = readJsonFile(biomeJsonPath);
+  biomeJson.$schema = `https://biomejs.dev/schemas/${biomeVersion}/schema.json`;
+  writeJsonFile(biomeJsonPath, biomeJson);
+  // biome-ignore lint/suspicious/noConsoleLog: <explanation>
+  console.log(
+    chalk.blue(
+      `biome.json has been updated with schema version ${biomeVersion}.`,
+    ),
+  );
+};
+
+const main = async () => {
+  const packageManager = detectPackageManager();
+  installBiomeLatest(packageManager);
+  updateBiomeSchema();
+  runFormart();
+  console.log(chalk.green("🎉 Biome updated successfully!"));
+};
+
+main();
