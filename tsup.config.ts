ea4a35dfdaaed9a77bb2c2f306eb2ff8a6e71cc0
r2 <121185412+ErwannRousseau@users.noreply.github.com>
2025-10-26 13:37:18 +0100

    Release/0.1.0 (#91)
    * fix: init lefhook
    
    * build(deps-dev): bump @types/node from 22.9.3 to 22.10.1 (#41)
    
    * build(deps-dev): bump @arethetypeswrong/cli from 0.17.0 to 0.17.1 (#42)
    
    * build(deps-dev): bump @types/node from 22.10.1 to 22.10.2 (#43)
    
    * build(deps-dev): bump chalk from 5.3.0 to 5.4.1 (#47)
    
    * build(deps-dev): bump typescript from 5.7.2 to 5.7.3 (#50)
    
    * build(deps-dev): bump @arethetypeswrong/cli from 0.17.1 to 0.17.3 (#51)
    
    * build(deps-dev): bump @changesets/cli from 2.27.10 to 2.27.12 (#55)
    
    * build(deps-dev): bump @types/node from 22.10.2 to 22.10.10 (#56)
    
    * build(deps-dev): bump @types/node from 22.10.10 to 22.12.0 (#57)
    
    * build(deps-dev): bump tsup from 8.3.5 to 8.3.6 (#58)
    
    * refactor: CLI with Commander.js and enhance user experience with @clack/prompts (#87)
    
    * docs(changeset): fix correct script execution for setup and update
    
    * RELEASING: Releasing 1 package(s)
    
    Releases:
      biome-config@0.0.6
    
    [skip ci]
    
    * feat: update biome to v2 (#85)
    
    * feat: update biome to v2
    
    * feat: add VS Code settings management and JSON merge utility (#86)
    
    * refactor: refactor CLI with Commander.js and enhance user experience with @clack/prompts
    
    * refactor: update things 🤠
    
    * fix: tsup config
    
    * feat: improve biome config
    
    * chore: bump biome version to v2.3.0 (#88)
    
    * chore: update configuration files and workflows for improved release process (#89)
    
    * chore: add changeset
    
    ---------
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   clean: true,
   dts: true,
   entryPoints: ["src/cli.ts", "src/setup.ts", "src/update.ts"],
+  external: [/^node:/, "commander", "chalk"],
   format: ["esm"],
   outDir: "dist",
+  shims: true,
 });
